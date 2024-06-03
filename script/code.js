f25578274d839be0a73f9c00da50b7e97f6ad04f
zakariyasalie <zakariya.salie07@gmail.com>
2024-06-03 12:11:26 +0200

    update

---
+++ script/code.js
@@ -0,0 +1,12 @@
+let first_number = 15;
+let second_number = 17;
+
+first_number = first_number + second_number;
+second_number = first_number - second_number;
+first_number = first_number - second_number;
+
+console.log("first_number =", first_number);
+console.log("second_number =", second_number);
+
+first_number = 17
+second_number = 15
